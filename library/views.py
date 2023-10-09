b7f9efe4914ea22a8a4cff008f0719a222e31b16
vinayak168 <130151636+vinayak168@users.noreply.github.com>
2023-10-09 14:17:16 +0530

    Add files via upload
    one file is remaining

---
+++ library/views.py
@@ -0,0 +1,239 @@
+from library.forms import IssueBookForm
+from django.shortcuts import redirect, render,HttpResponse
+from .models import *
+from .forms import IssueBookForm
+from django.contrib.auth import authenticate, login, logout
+from . import forms, models
+from datetime import date
+from django.contrib.auth.decorators import login_required
+# library/views.py
+import requests
+from django.shortcuts import render, redirect
+from .models import Book
+
+
+
+
+def index(request):
+    return render(request, "index.html")
+
+@login_required(login_url = '/admin_login')
+def add_book(request):
+
+   
+         # URL of the Frappe API
+    api_url = 'https://frappe.io/api/method/frappe-library?page=2&title=and'
+
+    try:
+        # Make an HTTP GET request to the Frappe API
+        response = requests.get(api_url)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Parse the JSON response
+            data = response.json()
+
+            # Iterate through the list of books in the response
+            for book_data in data['message']:
+                # Extract relevant information
+                title = book_data['title']
+                author = book_data['authors']
+                stock_quantity = 1  # Set an initial stock quantity
+
+                # Create a new Book object and save it to the database
+                book = Book(title=title, author=author, stock_quantity=stock_quantity)
+                book.save()
+
+            # Redirect to a success page or return a success response
+            return redirect('book_list')  # Replace with your book list view
+        else:
+            # Handle the case when the API request fails
+            return render(request, 'add_book.html')  # Create an error template
+    except Exception as e:
+        # Handle exceptions, e.g., network errors
+        return render(request, 'add_book.html')  # Create an error template
+       
+
+@login_required(login_url = '/admin_login')
+def view_books(request):
+    books = Book.objects.all()
+    return render(request, "view_books.html", {'books':books})
+
+@login_required(login_url = '/admin_login')
+def view_students(request):
+    students = Student.objects.all()
+    return render(request, "view_students.html", {'students':students})
+
+@login_required(login_url = '/admin_login')
+def issue_book(request):
+    form = forms.IssueBookForm()
+    if request.method == "POST":
+        form = forms.IssueBookForm(request.POST)
+        if form.is_valid():
+            obj = models.IssuedBook()
+            obj.student_id = request.POST['name2']
+            obj.isbn = request.POST['isbn2']
+            obj.save()
+            alert = True
+            return render(request, "issue_book.html", {'obj':obj, 'alert':alert})
+    return render(request, "issue_book.html", {'form':form})
+
+@login_required(login_url = '/admin_login')
+def view_issued_book(request):
+    issuedBooks = IssuedBook.objects.all()
+    details = []
+    for i in issuedBooks:
+        days = (date.today()-i.issued_date)
+        d=days.days
+        fine=0
+        if d>14:
+            day=d-14
+            fine=day*1
+        books = list(models.Book.objects.filter(isbn=i.isbn))
+        students = list(models.Student.objects.filter(user=i.student_id))
+        i=0
+        for l in books:
+            t=(students[i].user,students[i].user_id,books[i].name,books[i].isbn,issuedBooks[0].issued_date,issuedBooks[0].expiry_date,fine)
+            i=i+1
+            details.append(t)
+    return render(request, "view_issued_book.html", {'issuedBooks':issuedBooks, 'details':details})
+
+@login_required(login_url = '/student_login')
+def student_issued_books(request):
+    student = Student.objects.filter(user_id=request.user.id)
+    issuedBooks = IssuedBook.objects.filter(student_id=student[0].user_id)
+    li1 = []
+    li2 = []
+
+    for i in issuedBooks:
+        books = Book.objects.filter(isbn=i.isbn)
+        for book in books:
+            t=(request.user.id, request.user.get_full_name, book.name,book.author)
+            li1.append(t)
+
+        days=(date.today()-i.issued_date)
+        d=days.days
+        fine=0
+        if d>15:
+            day=d-14
+            fine=day*1
+        t=(issuedBooks[0].issued_date, issuedBooks[0].expiry_date, fine)
+        li2.append(t)
+    return render(request,'student_issued_books.html',{'li1':li1, 'li2':li2})
+
+@login_required(login_url = '/student_login')
+def profile(request):
+    return render(request, "profile.html")
+
+@login_required(login_url = '/student_login')
+def edit_profile(request):
+    student = Student.objects.get(user=request.user)
+    if request.method == "POST":
+        email = request.POST['email']
+        phone = request.POST['phone']
+        branch = request.POST['branch']
+        classroom = request.POST['classroom']
+        roll_no = request.POST['roll_no']
+
+        student.user.email = email
+        student.phone = phone
+        student.branch = branch
+        student.classroom = classroom
+        student.roll_no = roll_no
+        student.user.save()
+        student.save()
+        alert = True
+        return render(request, "edit_profile.html", {'alert':alert})
+    return render(request, "edit_profile.html")
+
+def delete_book(request, myid):
+    books = Book.objects.filter(id=myid)
+    books.delete()
+    return redirect("/view_books")
+
+def delete_student(request, myid):
+    students = Student.objects.filter(id=myid)
+    students.delete()
+    return redirect("/view_students")
+
+def change_password(request):
+    if request.method == "POST":
+        current_password = request.POST['current_password']
+        new_password = request.POST['new_password']
+        try:
+            u = User.objects.get(id=request.user.id)
+            if u.check_password(current_password):
+                u.set_password(new_password)
+                u.save()
+                alert = True
+                return render(request, "change_password.html", {'alert':alert})
+            else:
+                currpasswrong = True
+                return render(request, "change_password.html", {'currpasswrong':currpasswrong})
+        except:
+            pass
+    return render(request, "change_password.html")
+
+def student_registration(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        branch = request.POST['branch']
+        classroom = request.POST['classroom']
+        roll_no = request.POST['roll_no']
+        image = request.FILES['image']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password != confirm_password:
+            passnotmatch = True
+            return render(request, "student_registration.html", {'passnotmatch':passnotmatch})
+
+        user = User.objects.create_user(username=username, email=email, password=password,first_name=first_name, last_name=last_name)
+        student = Student.objects.create(user=user, phone=phone, branch=branch, classroom=classroom,roll_no=roll_no, image=image)
+        user.save()
+        student.save()
+        alert = True
+        return render(request, "student_registration.html", {'alert':alert})
+    return render(request, "student_registration.html")
+
+def student_login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            if request.user.is_superuser:
+                return HttpResponse("You are not a student!!")
+            else:
+                return redirect("/profile")
+        else:
+            alert = True
+            return render(request, "student_login.html", {'alert':alert})
+    return render(request, "student_login.html")
+
+def admin_login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            if request.user.is_superuser:
+                return redirect("/add_book")
+            else:
+                return HttpResponse("You are not an admin.")
+        else:
+            alert = True
+            return render(request, "admin_login.html", {'alert':alert})
+    return render(request, "admin_login.html")
+
+def Logout(request):
+    logout(request)
+    return redirect ("/")
